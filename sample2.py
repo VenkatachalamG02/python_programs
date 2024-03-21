498f08b9d46d529289fcb98fe8d593de9ac0da14
VenkatachalamG02 <venkatachalamg02@gmail.com>
2024-03-21 16:37:44 +0530

    Commit

--- sample2.py
+++ sample2.py
@@ -1,4 +1,11 @@
-def hello_user(name,age):
-    return f"Hello {name} who is {age} years old"
+# def hello_user(name,age):
+#     return f"Hello {name} who is {age} years old"
 
-print(hello_user('Venkat',22))
+# print(hello_user('Venkat',22))
+from pathlib import Path
+
+path = Path()
+files = path.glob('*.py')
+
+for file in files:
+    print(file)
