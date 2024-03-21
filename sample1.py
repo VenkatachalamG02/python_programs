609ca7ee3396447550e0f990792601b00396699a
VenkatachalamG02 <venkatachalamg02@gmail.com>
2024-03-21 15:30:47 +0530

    Third course started

---
+++ sample1.py
@@ -0,0 +1,11 @@
+name = 'John'
+age = 25
+isNewPatient = True
+
+
+# person = input('What is your name?')
+# color = input('What is your favorite color?')
+# print(person+' likes '+color+' color')
+
+course = 'Python for me'
+print('ythOn' in course)
---
+++ sample2.py
@@ -0,0 +1,4 @@
+def hello_user(name,age):
+    return f"Hello {name} who is {age} years old"
+
+print(hello_user('Venkat',22))
